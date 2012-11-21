6670c332f9aad74eed391339c90d5955d888bd2a
Dana M. Diederich <diederich@gmail.com>
2012-11-21 09:41:32 -0800

    basic code structure to support multiple types of serialization

--- ruby/lib/ipc_transit/serialize.rb
+++ ruby/lib/ipc_transit/serialize.rb
@@ -2,10 +2,34 @@ require 'rubygems'
 require 'json'
 
 def transit_freeze(args)
-    return args['message'].to_json
+    if args['e'].nil?
+        serialize_type = 'json'
+    else
+        serialize_type = args['e']
+    end
+    case serialize_type
+    when 'json'
+        return args['message'].to_json
+    when 'yaml'
+        puts 'yaml'
+    end
 end
 
 def transit_thaw(args)
-    return JSON.parse(args['serialized_message'])
+    if args['wire_headers'].nil?
+        serialize_type = 'json'
+    else
+        if args['wire_headers']['e'].nil?
+            serialize_type = 'json'
+        else
+            serialize_type = args['wire_headers']['e']
+        end
+    end
+    case serialize_type
+    when 'json'
+        return JSON.parse(args['serialized_message'])
+    when 'yaml'
+        puts 'yaml'
+    end
 end
 
